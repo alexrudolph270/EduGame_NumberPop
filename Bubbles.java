0022a495549ee4b5be6dd75bbe92f828437e94aa
alexrudolph96 <alexander.rudolph270@myci.csuci.edu>
2016-09-19 11:06:45 -0700

    Update Bubbles.java
    uploaded what should be the final ver of this, i think

--- Bubbles.java
+++ Bubbles.java
@@ -1,9 +1,48 @@
 import javax.swing.*;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-/**
- * Created by alex on 9/15/16.
- */
-public class Bubbles {
-    private int number;
+public class Bubble extends JButton
+{
+    private String number;  // Declares which bubble it is
     private ImageIcon image;
+    private int value;  // The value of its assigned number
+    private boolean onClick = false;
+
+    public Bubble(String newNum, ImageIcon newImage, int newVal)
+    {
+        this.number = newNum;
+        this.image = newImage;
+        this.value = newVal;
+        this.onClick = false;
+
+        //
+
+        this.setBackground(Color.WHITE);
+
+        this.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                if(!onClick)  // Determine if the icon is clicked and react accordingly
+                {
+                    onClick = true;
+                    setBackground(Color.green);
+                }
+                else
+                {
+                    onClick = false;
+                    setBackground(Color.darkGray);
+                }
+            }
+        });
+    }
+
+    // Return the value of the bubble
+    public int getVal()
+    {
+        return this.value;
+    }
 }
+
